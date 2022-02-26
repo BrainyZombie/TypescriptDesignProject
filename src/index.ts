5be1058b5a3c066c25f1aee09161afb953d92384
ashir basu <ashirbasu@gmail.com>
2022-02-26 23:30:35 +0530

    Set up google maps

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { User } from "./User";
-import { Company } from "./Company";
+/// <reference types="@types/google.maps" />
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-
-//new User
-const user = new User();
-const company = new Company();
-
-console.log(user);
-console.log(company);
+new google.maps.Map(document.getElementById('map'), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }   
+});
