5b23fc9f7061ab006522fce3f5479d3151dd2b0e
ashir basu <ashirbasu@gmail.com>
2022-02-26 22:53:13 +0530

    Added company class and instantiated

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { User } from "./User";
+import { Company } from "./Company";
+
 
 //new User
 const user = new User();
+const company = new Company();
+
 console.log(user);
+console.log(company);
